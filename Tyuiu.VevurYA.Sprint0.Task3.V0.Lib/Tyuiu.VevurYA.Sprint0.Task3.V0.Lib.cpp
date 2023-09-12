438377c8d9b248bd4553af168010b3da1b6de11f
Ян Вевюр <shadowfantomch@outlook.com>
2023-09-12 23:02:06 +0500

    Redone Task0, and started Task3

---
+++ Tyuiu.VevurYA.Sprint0.Task3.V0.Lib/Tyuiu.VevurYA.Sprint0.Task3.V0.Lib.cpp
@@ -0,0 +1,19 @@
+// Tyuiu.VevurYA.Sprint0.Task3.V0.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: This is an example of a library function
+
+
+class Service :public ISprint0Task3
+{
+
+	// Inherited via ISprint0Task2V0
+	virtual int SummV3(int a, int b, int c) override
+	{
+		return a + b + c;
+	}
+};
---
+++ Tyuiu.VevurYA.Sprint0.Task3.V0.Test/Tyuiu.VevurYA.Sprint0.Task3.V0.Test.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.VevurYA.Sprint0.Task3.V0.Lib/Tyuiu.VevurYA.Sprint0.Task3.V0.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestTask3
+{
+	TEST_CLASS(UnitTestTask3)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
